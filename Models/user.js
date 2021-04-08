b9d185184a071a11c62da261d3dd861cb26a4f8c
avigator003 <akshatlakshkar.3@gmail.com>
2021-04-09 05:26:11 +0530

    Authentication Setup Complete

---
+++ Models/user.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const crypto = require('crypto')
+const config = require('../config')
+const schemaOptions = {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  };
+const User = new Schema({
+    name:String,
+    userName: String,
+    emailAddress:{type:String, unique:true},
+    password: String,
+    admin: { type: Boolean, default: false },
+    phoneNumber:String,
+    upscAttempts:String,
+    additionalSubjects:String,
+    blocked: {type: Boolean, default: false},
+    verified: {type: Boolean, default: false},
+    account:Object,
+    target:Object,
+    registeredOn:Object,
+    subscriptionEndOn:Object
+}, schemaOptions)
+
+
+// crypto.createHmac('sha1', 'secret')
+//              .update('mypasswssord')
+//              .digest('base64')
+
+
+// create new User document
+User.statics.create = function(password,
+    name,
+  userName,
+  emailAddress,
+  phoneNumber,
+  upscAttempts,
+  additionalSubjects,
+  admin) {
+    const encrypted = crypto.createHmac('sha1', config.secret)
+                      .update(password)
+                      .digest('base64')
+
+    const user = new this({
+      name,
+    userName,
+    emailAddress,
+        password: encrypted,
+        phoneNumber,
+        upscAttempts,
+    additionalSubjects,
+    
+        admin
+    })
+
+    // return the Promise
+    return user.save()
+}
+
+// find one user by using username
+User.statics.findOneByEmailAddress = function(emailAddress) {
+    return this.findOne({
+        emailAddress
+    }).exec()
+}
+
+// verify the password of the User documment
+User.methods.verify = function(password) {
+    const encrypted = crypto.createHmac('sha1', config.secret)
+                      .update(password)
+                      .digest('base64')
+    console.log(this.password === encrypted)
+
+    return this.password === encrypted
+}
+
+User.methods.assignAdmin = function() {
+    this.admin = true
+    return this.save()
+}
+
+module.exports = mongoose.model('User', User)
